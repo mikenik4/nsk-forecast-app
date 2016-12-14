611b31855cbb66e0b00c32f228c1957afcac3e7b
Mikhail Shevchenko <mikenik4@gmail.com>
2016-12-14 23:16:53 +0700

    moved forecast list activity and fragment to package

--- app/src/main/java/com/mikenik/forecastmoxyapp/forecast_list/ForecastListActivity.java
+++ app/src/main/java/com/mikenik/forecastmoxyapp/forecast_list/ForecastListActivity.java
@@ -1,15 +1,13 @@
-package com.mikenik.forecastmoxyapp;
+package com.mikenik.forecastmoxyapp.forecast_list;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
+import com.mikenik.forecastmoxyapp.R;
 import com.mikenik.forecastmoxyapp.forecast_details.ForecastDetailsActivity;
 
 public class ForecastListActivity extends AppCompatActivity {
--- app/src/main/java/com/mikenik/forecastmoxyapp/forecast_list/ForecastListActivityFragment.java
+++ app/src/main/java/com/mikenik/forecastmoxyapp/forecast_list/ForecastListActivityFragment.java
@@ -1,10 +1,12 @@
-package com.mikenik.forecastmoxyapp;
+package com.mikenik.forecastmoxyapp.forecast_list;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.mikenik.forecastmoxyapp.R;
 
 /**
  * A placeholder fragment containing a simple view.
