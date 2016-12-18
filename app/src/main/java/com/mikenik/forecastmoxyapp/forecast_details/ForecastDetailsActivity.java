c2824b8de8c2ff2b2d1a6241ab18921ede548d36
Mikhail Shevchenko <mikenik4@gmail.com>
2016-12-18 16:04:01 +0700

    finished basic details screen design

--- app/src/main/java/com/mikenik/forecastmoxyapp/forecast_details/ForecastDetailsActivity.java
+++ app/src/main/java/com/mikenik/forecastmoxyapp/forecast_details/ForecastDetailsActivity.java
@@ -8,7 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.mikenik.forecastmoxyapp.R;
 import com.mikenik.forecastmoxyapp.api.xml.Forecast;
+import com.mikenik.forecastmoxyapp.api.xml.IntRange;
+import com.mikenik.forecastmoxyapp.api.xml.Wind;
 import com.mikenik.forecastmoxyapp.databinding.ActivityForecastDetailsBinding;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class ForecastDetailsActivity extends AppCompatActivity {
     private static final String FORECAST = ForecastDetailsActivity.class.getPackage() + ".Forecast";
@@ -25,6 +30,30 @@ public class ForecastDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_forecast_details);
-        Forecast forecast = (Forecast) getIntent().getSerializableExtra(FORECAST);
+        bind((Forecast) getIntent().getSerializableExtra(FORECAST));
+    }
+
+    private void bind(Forecast forecast) {
+        binding.date.setText(
+                SimpleDateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.SHORT)
+                        .format(forecast.getTimeDate()));
+        IntRange temperature = forecast.getTemperature();
+        binding.temperatureMin.setText(getString(R.string.min, temperature.getMin()));
+        binding.temperatureMax.setText(getString(R.string.max, temperature.getMax()));
+        IntRange heat = forecast.getHeat();
+        binding.heatMin.setText(getString(R.string.min, heat.getMin()));
+        binding.heatMax.setText(getString(R.string.max, heat.getMax()));
+        Wind wind = forecast.getWind();
+        binding.windMin.setText(getString(R.string.min, wind.getMin()));
+        binding.windMax.setText(getString(R.string.max, wind.getMax()));
+        String[] windDirections = getResources().getStringArray(R.array.item_wind_direction);
+        String windDirection = windDirections[wind.getDirection()];
+        binding.windDirection.setText(getString(R.string.details_wind_direction, windDirection));
+        IntRange pressure = forecast.getPressure();
+        binding.pressureMin.setText(getString(R.string.min, pressure.getMin()));
+        binding.pressureMax.setText(getString(R.string.max, pressure.getMax()));
+        IntRange relwet = forecast.getRelwet();
+        binding.relwetMin.setText(getString(R.string.min, relwet.getMin()));
+        binding.relwetMax.setText(getString(R.string.max, relwet.getMax()));
     }
 }
